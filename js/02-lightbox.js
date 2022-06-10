75e80171105f08f9f5fe3bb69cfdc204e64be7a5
AntonPlish <antonplish@gmail.com>
2022-06-10 10:35:30 +0300

    Commit

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -7,9 +7,9 @@ palletteContainer.insertAdjacentHTML('beforeend', createCardsGallery(galleryItem
 function createCardsGallery(galleryItems) {
     return galleryItems
         .map(({ preview, original, description }) => {
-            return `<a class="gallery__item" href="${original}">
+            return `<li><a class="gallery__item" href="${original}">
             <img class="gallery__image" src="${preview}" alt="${description}" />
-            </a>`;
+            </a></li>`;
         })
         .join('');
 };
